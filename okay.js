8ddb8d5fa91c8cfdad3245b8900538d6e077bc94
Jenne H <iv.castaire@gmail.com>
2018-02-19 11:51:21 -0800

    final modification, yay, that was quick

--- okay.js
+++ okay.js
@@ -1,4 +1,5 @@
 // setup
+"use strict";
 require('dotenv').config();
 
 const Snoowrap = require('snoowrap')
@@ -12,12 +13,26 @@ const s = new Snoowrap({
 	username: process.env.REDDIT_USER,
 	password: process.env.REDDIT_PASS
 });
-const ss = new Snoostorm(s);
+const client = new Snoostorm(s);
 
 // set stream options
+const sr = 'testingground4bots';
 const streamOpts = {
-	subreddit: 'testingground4bots',
-	results: 
+	subreddit: sr,
+	results: 30
 }
+
+//comment-check logic
+console.log("... starting 'er up ...");
+const comments = client.CommentStream(streamOpts);
+comments.on("comment", function(comment){
+	console.log(comment.author.name + ' posted: ' + comment.body);
+
+	if(comment.body.includes("(╯°□°）╯︵ ┻━┻")){
+		comment.reply("┬─┬ノ( º _ ºノ)");
+	};
+
+	console.log("ehue\n");
+});
 
 
